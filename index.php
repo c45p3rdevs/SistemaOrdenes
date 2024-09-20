c03b47384183ce7d811911de8bb7302b75e1fb55
Your Name <you@example.com>
2024-09-20 16:54:10 -0600

    PrimmerCommit

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['id'])) { //verificar que un usuario tiene una sesión activ
 <html>
 
 <head>
-	<title>PERS Orinoquía</title>
+	<title>DGSP</title>
 
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/main.css" rel="stylesheet">
